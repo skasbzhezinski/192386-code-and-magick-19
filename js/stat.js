25b371a45e43f9b023d290e37dbd641d8e0c5d74
Skasbzhezinski <skasbzhezinski@gmail.com>
2020-01-24 00:20:48 +0300

    add variables randomNum and columnColor

--- js/stat.js
+++ js/stat.js
@@ -8,6 +8,7 @@ var SHADOW_GAP = 10;
 var BAR_HEIGHT = 150; // = maxHeight
 var COLUMN_WIDTH = 40;
 var COLUMN_GAP = 50;
+var BOTTOM_GAP = 20;
 var sectionWidth = COLUMN_GAP + COLUMN_WIDTH;
 
 var renderCloud = function (ctx, x, y, color) {
@@ -19,15 +20,19 @@ window.renderStatistics = function (ctx, names, times) {
   renderCloud(ctx, CLOUD_X + SHADOW_GAP, CLOUD_Y + SHADOW_GAP, 'rgba(0, 0, 0, 0.7)');
   renderCloud(ctx, CLOUD_X, CLOUD_Y, '#fff');
 
-    ctx.font = '16px PT Mono';
-    ctx.fillStyle = '#000';
-    ctx.fillText('Ура вы победили!', CLOUD_X + 20, 36);
-    ctx.fillText('Список результатов:', CLOUD_X + 20, 57);
+  ctx.font = '16px PT Mono';
+  ctx.fillStyle = '#000';
+  ctx.fillText('Ура вы победили!', CLOUD_X + 20, 36);
+  ctx.fillText('Список результатов:', CLOUD_X + 20, 57);
 
   for (var i = 0; i < names.length; i += 1) {
+    var random = Math.round(Math.random() * 10) / 10;
+    var columnColor = `rgba(255, 0, 0, ${random})`;
+
     ctx.fillStyle = '#000';
-    ctx.fillText(names[i], CLOUD_X + COLUMN_GAP + sectionWidth * i, CLOUD_HEIGHT + CLOUD_Y - 20);
-    ctx.fillStyle = 'rgba(255, 0, 0, 1)';
-    ctx.fillRect(CLOUD_X + COLUMN_GAP + sectionWidth * i, CLOUD_HEIGHT + CLOUD_Y - BAR_HEIGHT - 16 - 20, COLUMN_WIDTH, BAR_HEIGHT);
+    ctx.fillText(names[i], CLOUD_X + COLUMN_GAP + sectionWidth * i, CLOUD_HEIGHT + CLOUD_Y - BOTTOM_GAP);
+    ctx.fillStyle = columnColor; // 'rgba(255, 0, 0, 1)';
+    ctx.fillRect(CLOUD_X + COLUMN_GAP + sectionWidth * i, CLOUD_HEIGHT + CLOUD_Y - BAR_HEIGHT - 16 - BOTTOM_GAP, COLUMN_WIDTH, BAR_HEIGHT);
+    console.log(columnColor);
   }
 };
