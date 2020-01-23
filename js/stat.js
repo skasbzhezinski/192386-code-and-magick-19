001e8375d0e60ebffdf601115228cace3c219b64
Skasbzhezinski <skasbzhezinski@gmail.com>
2020-01-23 19:17:56 +0300

    create a cloud rendering function

---
+++ js/stat.js
@@ -0,0 +1,14 @@
+'use strict';
+
+var CLOUD_WIDTH = 420;
+var CLOUD_HEIGHT = 270;
+
+var renderCloud = function (ctx, x, y, color) {
+  ctx.fillStyle = color;
+  ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
+};
+
+window.renderStatistics = function (ctx) {
+  renderCloud(ctx, 110, 20, 'rgba(0, 0, 0, 0.7)');
+  renderCloud(ctx, 100, 10, '#fff');
+};
