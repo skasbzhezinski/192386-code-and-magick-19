29e77ab43101778af8487fab0f3f3198d1c7eb13
Skasbzhezinski <skasbzhezinski@gmail.com>
2020-01-23 21:46:23 +0300

    create a loop to draw columns

--- js/stat.js
+++ js/stat.js
@@ -7,6 +7,7 @@ var CLOUD_Y = 10;
 var SHADOW_GAP = 10;
 var COLUMN_WIDTH = 40;
 var COLUMN_GAP = 50;
+var sectionWidth = COLUMN_GAP + COLUMN_WIDTH;
 
 var renderCloud = function (ctx, x, y, color) {
   ctx.fillStyle = color;
@@ -17,24 +18,41 @@ window.renderStatistics = function (ctx) {
   renderCloud(ctx, CLOUD_X + SHADOW_GAP, CLOUD_Y + SHADOW_GAP, 'rgba(0, 0, 0, 0.7)');
   renderCloud(ctx, CLOUD_X, CLOUD_Y, '#fff');
 
-  ctx.font = '16px PT Mono';
+  // ctx.font = '16px PT Mono';
+  var playerIndex = 0;
+  var playerName = 'Вы';
+
+  var players = ['Вы', 'Иван', 'Марина', 'Кекс'];
+
+  for (var i = 0; i < players.length; i += 1) {
+    ctx.fillStyle = '#000';
+    ctx.fillText(players[i], CLOUD_X + COLUMN_GAP + sectionWidth * i, 260);
+    ctx.fillStyle = 'rgba(255, 0, 0, 1)';
+    ctx.fillRect(CLOUD_X + COLUMN_GAP + sectionWidth * i, 50, COLUMN_WIDTH, 190);
+  }
+
+  /*
   ctx.fillStyle = '#000';
-  ctx.fillText('Вы', CLOUD_X + COLUMN_GAP, 260);
+  ctx.fillText(playerName, CLOUD_X + COLUMN_GAP + sectionWidth * playerIndex, 260);
   ctx.fillStyle = 'rgba(255, 0, 0, 1)';
-  ctx.fillRect(/* 150 */CLOUD_X + COLUMN_GAP, 50, COLUMN_WIDTH, 190);
+  ctx.fillRect(CLOUD_X + COLUMN_GAP, 50, COLUMN_WIDTH, 190);
+
+  playerIndex = 1;
+  playerName = 'Иван';
 
   ctx.fillStyle = '#000';
-  ctx.fillText('Иван', CLOUD_X + COLUMN_GAP * 2 + COLUMN_WIDTH, 260);
+  ctx.fillText('Иван', CLOUD_X + COLUMN_GAP + sectionWidth * playerIndex, 260);
   ctx.fillStyle = 'rgba(200, 50, 0, 1)';
-  ctx.fillRect(CLOUD_X + COLUMN_GAP * 2 + COLUMN_WIDTH, 100, COLUMN_WIDTH, 140);
+  ctx.fillRect(CLOUD_X + COLUMN_GAP + sectionWidth, 100, COLUMN_WIDTH, 140);
 
   ctx.fillStyle = '#000';
-  ctx.fillText('Марина', CLOUD_X + COLUMN_GAP * 3 + COLUMN_WIDTH * 2, 260);
+  ctx.fillText('Марина', CLOUD_X + COLUMN_GAP + sectionWidth * 2, 260);
   ctx.fillStyle = 'rgba(155, 100, 0, 1)';
-  ctx.fillRect(CLOUD_X + COLUMN_GAP * 3 + COLUMN_WIDTH * 2, 150, COLUMN_WIDTH, 90);
+  ctx.fillRect(CLOUD_X + COLUMN_GAP + sectionWidth * 2, 150, COLUMN_WIDTH, 90);
 
   ctx.fillStyle = '#000';
-  ctx.fillText('Кекс', CLOUD_X + COLUMN_GAP * 4 + COLUMN_WIDTH * 3, 260);
+  ctx.fillText('Кекс', CLOUD_X + COLUMN_GAP + sectionWidth * 3, 260);
   ctx.fillStyle = 'rgba(55, 150, 0, 1)';
-  ctx.fillRect(CLOUD_X + COLUMN_GAP * 4 + COLUMN_WIDTH * 3, 200, COLUMN_WIDTH, 40);
+  ctx.fillRect(CLOUD_X + COLUMN_GAP + sectionWidth * 3, 200, COLUMN_WIDTH, 40);
+  */
 };
