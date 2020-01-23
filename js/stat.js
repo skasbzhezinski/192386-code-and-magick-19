61ee1bb1a230a7cb7edafa5b9409c42d2a31d81b
Skasbzhezinski <skasbzhezinski@gmail.com>
2020-01-23 20:41:27 +0300

    set the position of the columns through constants

--- js/stat.js
+++ js/stat.js
@@ -21,20 +21,20 @@ window.renderStatistics = function (ctx) {
   ctx.fillStyle = '#000';
   ctx.fillText('Вы', CLOUD_X + COLUMN_GAP, 260);
   ctx.fillStyle = 'rgba(255, 0, 0, 1)';
-  ctx.fillRect(150, 50, 40, 190);
+  ctx.fillRect(/* 150 */CLOUD_X + COLUMN_GAP, 50, COLUMN_WIDTH, 190);
 
   ctx.fillStyle = '#000';
   ctx.fillText('Иван', CLOUD_X + COLUMN_GAP * 2 + COLUMN_WIDTH, 260);
   ctx.fillStyle = 'rgba(200, 50, 0, 1)';
-  ctx.fillRect(240, 100, 40, 140);
+  ctx.fillRect(CLOUD_X + COLUMN_GAP * 2 + COLUMN_WIDTH, 100, COLUMN_WIDTH, 140);
 
   ctx.fillStyle = '#000';
   ctx.fillText('Марина', CLOUD_X + COLUMN_GAP * 3 + COLUMN_WIDTH * 2, 260);
   ctx.fillStyle = 'rgba(155, 100, 0, 1)';
-  ctx.fillRect(330, 150, 40, 90);
+  ctx.fillRect(CLOUD_X + COLUMN_GAP * 3 + COLUMN_WIDTH * 2, 150, COLUMN_WIDTH, 90);
 
   ctx.fillStyle = '#000';
   ctx.fillText('Кекс', CLOUD_X + COLUMN_GAP * 4 + COLUMN_WIDTH * 3, 260);
   ctx.fillStyle = 'rgba(55, 150, 0, 1)';
-  ctx.fillRect(410, 200, 40, 40);
+  ctx.fillRect(CLOUD_X + COLUMN_GAP * 4 + COLUMN_WIDTH * 3, 200, COLUMN_WIDTH, 40);
 };
