675d389751e66574f8b8a35928b3a7b21992b214
Skasbzhezinski <skasbzhezinski@gmail.com>
2020-02-02 22:18:09 +0300

    add the attribute minlength=2 to the input element, add closing the window when pressing the Esk key

--- js/setup.js
+++ js/setup.js
@@ -74,10 +74,30 @@ var setupClose = setup.querySelector('.setup-close');
 
 setupOpen.addEventListener('click', function () {
   setup.classList.remove('hidden');
+
+  document.addEventListener('keydown', function (evt) {
+    if (evt.key === 'Escape') {
+      setup.classList.add('hidden');
+    }
+  });
 });
+
+// ========= отладка ========= //
 console.log(setup);
 console.log(setupOpen);
 
 setupClose.addEventListener('click', function () {
   setup.classList.add('hidden');
 });
+
+// var userNameInput = setup.querySelector('.setup-user-name');
+
+// userNameInput.addEventListener('invalid', function (evt) {
+//   if (userNameInput.validity.tooShort) {
+//     userNameInput.setCustomValidity('Имя должно состоять минимум из 2-х символов');
+//   } else if (userNameInput.validity.tooLong) {
+//     userNameInput.setCustomValidity('Имя не должно превышать 25-ти символов');
+//   } else if (userNameInput.validity.valueMissing) {
+//     userNameInput.setCustomValidity('Обязательное поле');
+//   }
+// });
